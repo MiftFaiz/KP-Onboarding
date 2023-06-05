adb3036983ac6d994006bbd6162514127a5b738a
MiftFaiz <siremadu132@gmail.com>
2023-06-05 10:53:55 +0700

    Day1 End

--- Day1/day1.py
+++ Day1/day1.py
@@ -58,7 +58,100 @@ app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 # Styling komponen menggunakan Bootstrap
 card_style = {'width': '25rem', 'margin': '1rem'}
 
-app.layout = dbc.Container([
+search_form = dbc.Row(
+    [
+        dbc.Col(dbc.Input(type="search", placeholder="Search"), width=8),
+        dbc.Col(
+            dbc.Button("Search", color="success", className="ml-2", outline=True),
+            width=2,
+        ),
+    ],
+    className="ml-auto flex-nowrap mt-3 mt-md-0 align-items-center",  # Add custom CSS classes
+)
+
+navbar = dbc.NavbarSimple(
+    children=[
+        dbc.NavItem(dbc.NavLink("Home", href="#")),
+        dbc.NavItem(dbc.NavLink("Link", href="#")),
+        dbc.DropdownMenu(
+            children=[
+                dbc.DropdownMenuItem("Action", href="#"),
+                dbc.DropdownMenuItem("Another action", href="#"),
+                html.Div(className="dropdown-divider"),
+                dbc.DropdownMenuItem("Something else here", href="#"),
+            ],
+            nav=True,
+            in_navbar=True,
+            label="Dropdown",
+        ),
+        dbc.NavItem(dbc.NavLink("Disabled", disabled=True)),
+        dbc.NavItem(dbc.NavLink(search_form)),
+    ],
+    brand="Team 2 On Boarding",
+    brand_href="#",
+    color="primary",
+    dark=True,
+)
+
+footer = dbc.Navbar(
+    dbc.Container(
+        [
+            dbc.Row(
+                [
+                    dbc.Col(
+                        dbc.Nav(
+                            [
+                                dbc.NavItem(dbc.NavLink("About", href="#")),
+                            ],
+                            
+                            navbar=True,
+                        ),
+                    ),
+                    dbc.Col(
+                        dbc.Nav(
+                            [
+                                dbc.NavItem(dbc.NavLink("Terms of Service", href="#")),
+                            ],
+                           
+                            navbar=True,
+                        ),
+                    ),
+                    dbc.Col(
+                         dbc.Nav(
+                            [
+                                dbc.NavItem(dbc.NavLink("Privacy Policy", href="#")),
+                            ],
+                            navbar=True,
+                        ),
+                    ),
+                ],
+                align="center",
+            ),
+             dbc.Row(
+                    dbc.Col(html.P("© 2023 Your Company. All rights reserved.", className="text-muted")),
+            )
+        ]
+    ),
+    color="light",
+    className="mt-8",
+    dark=False,
+    sticky="bottom",
+)
+
+
+app.layout = dbc.Container(
+     style={
+        "background-image": "url('/Data/bg.jpg')",
+        "background-size": "cover",
+        "background-position": "center",
+        "height": "100vh",
+    },
+
+    children=[
+    
+    navbar,
+    
+    html.Div(className="mt-4"),
     dbc.Row([
         dbc.Col(html.H1('PNBP Fraud Detection Dashboard', className='text-center mb-4'), width=12)
     ]),
@@ -71,41 +164,54 @@ app.layout = dbc.Container([
                     html.P(f"Daerah dengan Kawasan Hutan Besar dan PNBP Kecil: {', '.join(daerah_anomali_kawasan_hutan_pnbp)}"),
                     dcc.Graph(figure=fig_kawasan_hutan_pnbp)
                 ])
-            ], style=card_style)
-        ], width=6),
-        
+            ], className="mx-auto", style={'width': '75%'})
+        ], width=12),
+
+    ], className='mb-4'),
+
+    dbc.Row([
         dbc.Col([
             dbc.Card([
                 dbc.CardHeader(html.H2('Analisis 2: Pengusaha dengan PNBP Lebih Rendah')),
                 dbc.CardBody([
-                    html.P(f"Pengusaha dengan PNBP Lebih Rendah: {', '.join(pengusaha_pnbp_rendah)}")
+                    html.P(f"Pengusaha dengan PNBP Lebih Rendah: {', '.join(pengusaha_pnbp_rendah)}"),
+                    dcc.Graph(figure=fig_produksi_pnbp)
                 ])
-            ], style=card_style)
-        ], width=6)
+            ], className="mx-auto", style={'width': '75%'})
+        ], width=12)
     ], className='mb-4'),
-    
+
     dbc.Row([
         dbc.Col([
             dbc.Card([
                 dbc.CardHeader(html.H2('Analisis 3: Daerah dengan Produksi Besar namun PNBP Kecil')),
                 dbc.CardBody([
-                    html.P(f"Daerah dengan Produksi Besar dan PNBP Kecil: {', '.join(daerah_anomali_produksi_pnbp)}"),
+                    html.P(f"Daerah dengan Produksi Besar dan PNBP Kecil: "),
                     dcc.Graph(figure=fig_produksi_pnbp)
                 ])
-            ], style=card_style)
-        ], width=6),
-        
+            ], className="mx-auto", style={'width': '75%'})
+        ], width=12),
+    ], className='mb-4'),
+
+    dbc.Row([
         dbc.Col([
             dbc.Card([
                 dbc.CardHeader(html.H2('Analisis 4: Daerah dengan Volume dan Value Ekspor Besar namun PNBP Kecil')),
                 dbc.CardBody([
-                    html.P(f"Daerah dengan Volume dan Value Ekspor Besar dan PNBP Kecil: {', '.join(daerah_anomali_ekspor_pnbp)}"),
+                    html.P(f"Daerah dengan Volume dan Value Ekspor Besar dan PNBP Kecil: "),
                     dcc.Graph(figure=fig_ekspor_pnbp)
                 ])
-            ], style=card_style)
-        ], width=6)
-    ])
-], fluid=True)
+            ], className="mx-auto", style={'width': '75%'})
+        ], width=12)
+    ]),
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+    footer,
+
+],
+fluid=True)
+
+if __name__ == "__main__":
+    try:
+        app.run_server(debug=True)
+    except Exception as e:
+        print(f"Error: {e}")
